d1a5aa6e6154f31fb6f3308d9c9cfd5ec3268a9b
Stefan <stefantimosenco@gmail.com>
2016-11-13 21:26:52 +0000

    added public folder

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 var express = require('express');
+var path = require("path");
+var moment = require('moment');
 
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.set("views",path.join(__dirname,'public'));
+app.set("view engine","jade");
+app.use(express.static,path.join(__dirname,'public'));
+
+app.get('/',function(req,res,next){
+  res.render("index",{action:"Unix Timestamp and Natural Language"});
+  next();  
 });
+
+
+app.get('/:date', function (req, res) {
+  var reqDate = req.params.date;
+  var isValidDate = moment(reqDate,null,true).isValid();
+  var msg;
+  if(isValidDate){
+    var unixDate =  new Date(reqDate).getTime()/1000;
+    var naturalDate = reqDate;
+    var resObj = {
+      "unix": unixDate,
+      "natural": naturalDate
+    };
+    msg = JSON.stringify(resObj);
+  } else {
+    msg = "Invalid date format!";
+  }
+  res.render("index",{action:msg});
+});
+
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
