d950cd03f3368766d343d592b254f1f2867d1777
Stefan <stefantimosenco@gmail.com>
2016-11-13 21:45:29 +0000

    modified server.js and modified folder strucure of public

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ var moment = require('moment');
 
 var app = express();
 
-app.set("views",path.join(__dirname,'public'));
+app.set("views",path.join(__dirname,'/public/template'));
 app.set("view engine","jade");
-app.use(express.static,path.join(__dirname,'public'));
+app.use(express.static(path.join(__dirname,'/public/template')));
 
 app.get('/',function(req,res,next){
   res.render("index",{action:"Unix Timestamp and Natural Language"});
